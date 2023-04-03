5482c1640a947cca3f6efd481edfe90c00a685cc
HaithamBk <engineerhmb007@gmail.com>
2023-04-03 02:14:08 +0200

    -Creating a website that gets api data from another website , and displays everyday a new image with new title and new explanation.

--- AstronomyImage.py
+++ AstronomyImage.py
@@ -3,33 +3,34 @@
 import streamlit as st
 import requests
 
-st.set_page_config("wide")
-st.title("Galaxy by the lake")
-st.image("pic.jpg")
-desc = f"""
-dasdasdasdsadasdasdsadasdasd
-asdsadsadsadsadasdsadsadasdsd
-asdsadsadsadasdasdsadasdasdsad
-adsadsadsadasdasdasdsadasdasdas
-"""
-st.write(desc)
-
 # My Nasa Api Key
 api_key = "glLyS2rRlWXGzFtyOQPM1yFiRHy3ADokHV1F46ME"
 
 # URLS
 core_url = "https://api.nasa.gov/planetary/apod"
-# we have parameters api_key = DEMO_KEY and date =today from the website query parameters
-param_url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date=today"
+# we have parameters api_key = DEMO_KEY and
+# query example : https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+param_url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 print(param_url)
 
 # get the HTTP response of the url
 response = requests.get(param_url)
-print(response)
+print(response)  # --> <Response [200]>
+
+# get the data of this response as dictionary data structure
+data = response.json()
+print(data)
+
+# get the image title ,image  url , and image explanation
+title = data["title"] # provide the key of dictionary data to get its value and store it in variable
+explanation = data["explanation"] # provide the key of dictionary data to get its value and store it in variable
+img_url  = data["url"] # provide the key of dictionary data to get its value and store it in variable
+print(title)
+print(img_url)
+print(explanation)
+
 
 # IMAGE SECTION
-# get the image url
-img_url = "https://apod.nasa.gov/apod/image/2304/Ring_HubbleSchmidt_1548.jpg"
 # get the http response of the image
 img_response = requests.get(img_url)
 print(img_response)  # if response <Response [200]? means you dont need to put headers
@@ -38,3 +39,9 @@ img_code = img_response.content
 # create the image from code as file.jpg
 with open("pic.jpg" , "wb") as file:
     file.write(img_code)
+
+# creating the portofolio web using streamlit
+st.set_page_config("wide")
+st.title(title)
+st.image("pic.jpg")
+st.write(explanation)
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # What is this project?
 This project is a portfolio website that displays an astronomy image of the day.
-Each day it displays a new image and a discription.
+Each day it displays a new image and with its title and description.
